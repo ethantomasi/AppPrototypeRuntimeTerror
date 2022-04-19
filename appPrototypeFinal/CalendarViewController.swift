d85bba20c9af1e8848fc72c881b8629644b41a3e
efan <ethantomasi18@gmail.com>
2022-04-19 11:28:18 -0600

    Calendar Update

--- appPrototypeFinal/CalendarViewController.swift
+++ appPrototypeFinal/CalendarViewController.swift
@@ -1,27 +1,19 @@
 //
-//  ViewController.swift
+//  CalendarViewController.swift
 //  appPrototypeFinal
 //
-//  Created by Ethan Tomasi on 4/12/22.
+//  Created by Ethan Tomasi on 4/19/22.
 //
 
 import UIKit
 import CalendarKit
 
-class ViewController: UIViewController {
-
+class CalendarViewController: DayViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "Calendar"
         
     }
-    
-    
-    
-    
-    
-    
-    
-     
 }
-
